e604d9d66405136f8cf79a98ec82510aca6dd28a
aguscorvo <aguscorvo@gmail.com>
2021-02-23 14:26:08 -0300

    Visibility Change

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -2,6 +2,7 @@ class AutoPause {
   constructor() {
     this.threshold = 0.25;
     this.handleIntersection = this.handleIntersection.bind(this);
+    this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
   }
   run(player) {
     this.player = player;
@@ -10,12 +11,23 @@ class AutoPause {
     });
 
     observer.observe(this.player.media);
+
+    document.addEventListener('visibilitychange', this.handleVisibilityChange);
   }
 
   handleIntersection(entries) {
     const entry = entries[0];
 
     const isVisible = entry.intersectionRatio >= this.threshold;
+    if (isVisible) {
+      this.player.play();
+    } else {
+      this.player.pause();
+    }
+  }
+
+  handleVisibilityChange() {
+    const isVisible = document.visibilityState === 'visible';
     if (isVisible) {
       this.player.play();
     } else {
